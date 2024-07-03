49981610fa6b7f31eb644f691adececc9015033e
DanielUmetor <dnumetor@gmail.com>
2024-07-03 17:45:09 +0200

    views

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/WorkView.vue'
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+import MainView from '../views/MainView.vue'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'main',
+            component: MainView
+        }
+    ],
+    scrollBehavior(to, from, savedPosition) {
+        if (to.hash && to.hash == '#landing-page') {
+            return {
+                top: 0,
+                behavior: window.matchMedia('(prefers-reduced-motion: no-preference)').matches ? 'smooth' : 'instant'
+            }
+        }
+        if (to.hash) {
+            return {
+                el: to.hash,
+                top: isScrollingUp(to.hash) ? getOffsetHeight() : 0,
+                behavior: window.matchMedia('(prefers-reduced-motion: no-preference)').matches ? 'smooth' : 'instant'
+            }
+        }
+    },
 })
+
+let isScrollingUp = (elem_id) => {
+  const elem = document.querySelector(elem_id)
+  const toScrollPos = elem.getBoundingClientRect().top
+
+  return toScrollPos < 0
+}
+
+let getOffsetHeight = () => {
+  const nav = document.querySelector('nav')
+  const navHeight = nav.getBoundingClientRect().height + 25
+
+  return navHeight
+}
 
 export default router
